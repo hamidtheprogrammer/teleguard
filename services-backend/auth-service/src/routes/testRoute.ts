a8a08c0057963ff92fb24ccf0edfa1638cfc9e31
HAMID <owoyemiameed@gmaiil.com>
2024-11-12 22:54:49 +0000

    updating workflow

--- services-backend/auth-service/src/routes/testRoute.ts
+++ services-backend/auth-service/src/routes/testRoute.ts
@@ -3,6 +3,6 @@ import { test } from "../controllers/testController";
 
 const testRoute = Router();
 
-testRoute.route("/").get(test);
+testRoute.route("/test").get(test);
 
 export default testRoute;
